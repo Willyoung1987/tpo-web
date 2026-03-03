5aa9f3d73b32e8418d870cd3185d9cd48cecf5b8
Willyoung1987 <yangweirong1987@hotmail.com>
2026-03-03 19:31:19 +0800

    app.py
    fig path change

--- tpo/app.py
+++ tpo/app.py
@@ -240,7 +240,7 @@ st.markdown("""
 「TPO + 示例股票代码 + 微信昵称」
 """)
 
-st.image("static/QRcode.png", caption="微信扫码付款（支持红包/转账）", width=300)
+st.image("https://github.com/Willyoung1987/tpo-web/tree/main/tpo/static/QRcode.png", caption="微信扫码付款（支持红包/转账）", width=300)
 
 st.markdown("""
 付款成功后截图发微信：**你的微信号**  
@@ -248,3 +248,4 @@ st.markdown("""
 """)
 
 st.caption("数据来源于Tushare | 仅供学习交流 | 如有疑问加微信：你的微信号")
+
